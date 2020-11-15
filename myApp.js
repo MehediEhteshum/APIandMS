55fe6d0a0cb2d75c7c75aa34395e6708eeb31442
Mehedi Ehteshum <me69oshan@gmail.com>
2020-11-14 20:48:49 -0700

    middleware: logger; chain middleware: time server; getting route params, query params; parsing post;

--- myApp.js
+++ myApp.js
@@ -1,7 +1,47 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 // console.log("Hello World");
 let rootPath = "/";
+
+// parse post requests.
+let parsePost = bodyParser.urlencoded({extended: false});
+app.use(parsePost);
+
+// middleware: simple logger function.
+let loggerFunc = function(req, res, next){
+  console.log(req.method+" "+req.path+" - "+req.ip);
+  next();
+};
+app.use(loggerFunc);
+
+// chain middleware: time server.
+let timeCatcherFunc = function(req, res, next){
+  req.time = new Date().toString();
+  next();
+};
+let timeHandler = function(req, res){
+  res.json({time: req.time});
+};
+let currTimePath = "/now";
+app.get(currTimePath, timeCatcherFunc, timeHandler);
+
+// echo server.
+let echoHandler = function(req, res){
+  res.json({echo: req.params.word});
+};
+let echoPath = "/:word/echo";
+app.get(echoPath, echoHandler);
+
+// get query params.
+let nameHandler = function(req, res){
+  res.json({name: req.query.first+" "+req.query.last});
+};
+let namePoster = function(req, res){
+  res.json({name: req.body.first+" "+req.body.last});
+};
+let namePath = "/name";
+app.route(namePath).get(nameHandler).post(namePoster);
 
 let stylePath = __dirname+"/public";
 let styleFunc = express.static(stylePath); // middleware function for styling.
@@ -23,40 +63,5 @@ app.get(jsonPath, function(req, res){
     res.json({"message": "Hello json"});
   }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
  module.exports = app;
